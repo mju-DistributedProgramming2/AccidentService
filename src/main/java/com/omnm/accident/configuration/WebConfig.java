f617a8819789948225aaaca69ba53fc338108d66
zoid79 <zidwkd00@naver.com>
2023-12-06 19:48:09 +0900

    delete unused Entity

--- src/main/java/com/omnm/accident/configuration/WebConfig.java
+++ src/main/java/com/omnm/accident/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package com.omnm.accident;
+package com.omnm.accident.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
